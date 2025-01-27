6422516cdc43f867a38419283a93ea636b91093a
haldor-dev <support@haldor.xyz>
2025-01-27 14:35:24 +1300

    fix: add url scheme

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -4,5 +4,5 @@ export const postUrl = bytebinUrl + 'post';
 function getDataSubdomain(): string {
   let domainParts = document.location.host.split(".");
   domainParts[0] = "bytebin";
-  return domainParts.join(".") + "/";
+  return "//" + domainParts.join(".") + "/";
 }
